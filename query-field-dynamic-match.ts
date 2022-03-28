90b64c4643a48cb1b1ac98b43467e16bd2443b39
fimac <fiona@cipherstash.com>
2022-03-29 10:07:34 +1100

    add dynamic match and field dynamic match examples

---
+++ query-field-dynamic-match.ts
@@ -0,0 +1,22 @@
+import { Stash } from "@cipherstash/stashjs"
+import { movieSchema } from "./example-schema";
+import { displayResults } from "./query-helper";
+
+async function queryCollection() {
+  try {
+    const stash = await Stash.connect()
+    const movies = await stash.loadCollection(movieSchema)
+
+    let queryResult = await movies.query(
+      movie => movie.allTextFieldDynamicMatch.match("title", "life"),
+      { limit: 10 }
+    )
+    displayResults(queryResult, "Match: 'life'")
+
+  } catch (err) {
+    console.error(err)
+    console.error(`Could not query collection! Reason: ${JSON.stringify(err)}`)
+  }
+}
+
+queryCollection()
